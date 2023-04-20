2b59e8681516e350e00e92417401fa602aab7149
Laclcr <laclcr@floridauniversitaria.es>
2023-04-20 16:56:42 +0200

    Prueba

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace AEV7
         [STAThread]
         static void Main()
         {
+            //cAMBIOS REALIZADOS POR LAIA
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
